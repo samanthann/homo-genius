334fa350e7286804b74f85cce15b70b55b1d825c
samanthann <samantha_mott@outlook.com>
2018-12-03 20:53:41 -0700

    Responsive changes

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,26 @@
 $(document).ready(function(){
     $('#newsletterModal').on('hidden.bs.modal', function () {
+        
         window.location.href = 'Index.html';
+
       });
+
+      $('#newsletter-submit').on('click', function() {
+          var email = $('#email-input').val();
+          if(ValidateEmail(email)){
+            $('#newsletterModal').modal('show');
+          }
+          else{
+              alert("Please enter a valid email address")
+          }
+      })
 })
+
+function ValidateEmail(mail) 
+{
+ if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail))
+  {
+    return true;
+  }
+    return false;
+}
